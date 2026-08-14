03c0b99dbbc87c6c0e47b590fb82c11581ede889
BuildON-Tech <info@buildonhq.org>
2026-08-14 16:04:59 +0100

    first commit

---
+++ knexfile.ts
@@ -0,0 +1,26 @@
+import type { Knex } from "knex";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Update with your config settings.
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST as string,
+      user: process.env.DB_USER as string,
+      password: process.env.DB_PASSWORD as string,
+      database: process.env.DB_NAME as string,
+    },
+    migrations: {
+      directory: "./src/database/migrations",
+    },
+    seeds: {
+      directory: "./src/database/seeds",
+    },
+  },
+};
+
+export default config;
---
+++ src/database/migrations/20260814143854_create_initial_tables.ts
@@ -0,0 +1,40 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("users", (table) => {
+    table.uuid("id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").unique().notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable("wallets", (table) => {
+    table.uuid("id").primary();
+    table.uuid("user_id").references("id").inTable("users").onDelete("CASCADE");
+    table.string("last_name").notNullable();
+    table.string("email").unique().notNullable();
+    table.decimal("balance", 15, 2).defaultTo(0.0).notNullable();
+    table.timestamps(true, true);
+  });
+
+  await knex.schema.createTable("transactions", (table) => {
+    table.uuid("id").primary();
+    table
+      .uuid("wallet_id")
+      .references("id")
+      .inTable("wallets")
+      .onDelete("CASCADE");
+    table.enum("type", ["CREDIT", "DEBIT", "TRANSFER"]).notNullable();
+    table.decimal("amount", 15, 2).notNullable();
+    table.string("reference").unique().notNullable();
+    table.enum("status", ["PENDING", "SUCCESS", "FAILED"]).defaultTo("PENDING");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTableIfExists("transactions");
+  await knex.schema.dropTableIfExists("wallets");
+  await knex.schema.dropTableIfExists("users");
+}
