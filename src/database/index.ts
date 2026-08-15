647bbba3e2ef7c4dcf44bbed9698d5aa4cb04d16
sam-aydev <samadet0001@gmail.com>
2026-08-15 18:42:00 +0100

    fix: dynamically load knex config based on environment

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,6 @@
 import knex from "knex";
 import config from "../../knexfile";
 
-const db = knex(config.development);
+const environment = process.env.NODE_ENV || "development";
+const db = knex(config[environment]);
 export default db;
