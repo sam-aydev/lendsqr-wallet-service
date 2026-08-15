86e9759a301513c9a9c03d6b859c817475adf761
sam-aydev <samadet0001@gmail.com>
2026-08-15 16:12:52 +0100

    chore: added production database configuration for Railway deployment; fix: update moduleResolution to node to satisfy typescript compiler rules

--- knexfile.ts
+++ knexfile.ts
@@ -21,6 +21,20 @@ const config: { [key: string]: Knex.Config } = {
       directory: "./src/database/seeds",
     },
   },
+
+  production: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DB_HOST as string,
+      user: process.env.DB_USER as string,
+      password: process.env.DB_PASSWORD as string,
+      database: process.env.DB_NAME as string,
+    },
+    pool: {
+      min: 2,
+      max: 10
+    }
+  },
 };
 
 export default config;
